4bd5afbf9f52edafa07c56138cbe3aeef252e6d1
vivekk01 <vivekbhardwaj0222@gmail.com>
2022-11-06 14:03:04 +0530

    okfinee

--- myapp1/src/Pages/blog.js
+++ myapp1/src/Pages/blog.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Ayodhya from '../Gallery/ayodhya.png'
 import Akshardham from '../Gallery/akshardham.png'
-import Delhi from '../Gallery/delhi.jpg'
 import Rishikesh from '../Gallery/OIPP.PNG'
 import ArrowOutwardSharpIcon from '@mui/icons-material/ArrowOutwardSharp';
 function Blog() {
@@ -11,7 +10,6 @@ function Blog() {
      <div className='boxes'>
         <div className='box-1'><img  className='img-data-blog' src={Ayodhya} alt='img' /><h4 className='name-dataa'>Ayodhya Blog<ArrowOutwardSharpIcon /></h4></div>
         <div className='box-1'><img  className='img-data-blog' src={Akshardham} alt='img' /><h4 className='name-dataa'>Akshardham Blog<ArrowOutwardSharpIcon /></h4></div>
-        <div className='box-1'><img className='img-data-blog' src={Delhi} alt='img' /><h4 className='name-dataa'>Kolkata-Delhi Blog<ArrowOutwardSharpIcon /></h4></div>
         <div className='box-1'><img className='img-data-blog' src={Rishikesh} alt='img' /><h4 className='name-dataa'>Rishikesh Blog<ArrowOutwardSharpIcon /></h4></div>
         </div>
     </div>
