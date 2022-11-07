6bf6f9293d98773e4981dc313676d59a2d5d72aa
vivekk01 <vivekbhardwaj0222@gmail.com>
2022-11-07 20:21:37 +0530

    heyy

--- myapp1/src/acc/gallery.js
+++ myapp1/src/acc/gallery.js
@@ -55,7 +55,7 @@ const postComment = async(e)=>{
     </div>
     
        <div className='view-comments'> 
-      <h5 className='allinonee' style={{textAlign:'center',color:'green'}}>Comment Section</h5><hr /><br />
+      <h5 className='allinonee' style={{textAlign:'center',color:'green',textDecoration:'underline'}}>Comment Section</h5><br />
       {
         comment.map((item,id)=>
           <Comments key={id} value={item} />
