fc438c65aa711978d55e5756279f8df1749d135a
vivekk01 <vivekbhardwaj0222@gmail.com>
2022-11-08 16:10:42 +0530

    okkkdone

--- myapp1/src/Pages/page2.js
+++ myapp1/src/Pages/page2.js
@@ -6,6 +6,8 @@ function Page2() {
     <div className='page2'>
     <div className='hero-text'>
         <img className='hero-img' src={IMG} alt='img' /> 
+        </div>
+        <div>
         <h1 className='hero-name hero-items'>Myself, Vivek Bhardwaj!👋</h1>
         <h3 className='hero-items hero-last-name'>I'm a FullStack Web Developer.</h3>
         <p className='info'>I 🧡 programming and I make end to end web apps.</p>
